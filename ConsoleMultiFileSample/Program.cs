f34b670a30efe956827ebec0d53beab309b9ea22
JBraunsmaJr <jonbraunsma@gmail.com>
2022-02-21 18:57:55 -0500

    Implemented multi-file lang support

---
+++ ConsoleMultiFileSample/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Globalization;
+using ConsoleMultiFileSample;
+
+Console.WriteLine(Localizer.Get(LocalizationResource.HELLO));
+Console.WriteLine(Localizer.Get(LocalizationResource.HELLO, LocalizationLanguage.de_DE));
+Console.WriteLine(Localizer.Get(LocalizationResource.HELLO, CultureInfo.CurrentCulture));
+Console.WriteLine(Localizer.Get(LocalizationResource.SAMPLE, LocalizationLanguage.de_DE));
+
+Console.WriteLine("Hello, World!");
