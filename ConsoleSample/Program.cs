74fea474ea9abbc20f6da83b5995d2c596cda47b
Badger <jonBraunsma@gmail.com>
2022-02-21 19:22:42 -0500

    Implemented multi-file lang support (#4)
    * Implemented multi-file lang support
    
    * Minor tweaks to examples and readme

---
+++ ConsoleSample/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+using ConsoleSample;
+
+Console.WriteLine(Localizer.Get(LocalizationResource.HELLO));
+Console.WriteLine(Localizer.Get(LocalizationResource.HELLO, LocalizationLanguage.de_DE));
+Console.WriteLine(Localizer.Get(LocalizationResource.HELLO, CultureInfo.CurrentCulture));
--- LocalizationGenerator/LocalizationGenerator.cs
+++ LocalizationGenerator/LocalizationGenerator.cs
@@ -1,210 +1,271 @@
 ﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using Microsoft.CodeAnalysis;
 using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
 
 namespace LocalizationGenerator
 {
     [Generator]
     public class LocalizationGenerator : ISourceGenerator
     {
-        private GeneratorExecutionContext _context;
+        private const string ResourceEnum = "LocalizationResource";
+        private const string LanguageEnum = "LocalizationLanguage";
+        private Regex _fileRegex = new Regex("([a-z]{2}-[A-Z]{2})%*.json");
 
-        private const string LANGUAGE_ENUMS = "%LANGS%";
-        private const string ENUMS = "%ENUMS%";
-        private const string SUPPORTED_LANGUAGES = "%SUPPORTED_LANGUAGES%";
-        private const string NAMESPACE = "%NAMESPACE%";
-        private const string ENTRIES = "%ENTRIES%";
+        /// <summary>
+        /// Key: Resource Name -->
+        ///     Key: Language Code -->
+        ///         Value: Translated Text
+        /// </summary>
+        private ConcurrentDictionary<string, Dictionary<string, string>> _output =
+            new ConcurrentDictionary<string, Dictionary<string, string>>();
+
+        /// <summary>
+        /// All languages that have been discovered 
+        /// </summary>
+        private ConcurrentBag<string> _languages = new ConcurrentBag<string>();
         
-        private const string Template = 
-@"
-namespace %NAMESPACE%;
+        /// <summary>
+        /// Tracks a resource key to the languages it can translate to
+        /// </summary>
+        ConcurrentDictionary<string, HashSet<string>> _tracking = new ConcurrentDictionary<string, HashSet<string>>();
+        
+        private GeneratorExecutionContext _context;
+        
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            
+        }
+
+        private void ReportError(string message, DiagnosticSeverity severity = DiagnosticSeverity.Error)
+        {
+            _context.ReportDiagnostic(
+                Diagnostic.Create(new DiagnosticDescriptor("SI001", 
+                        message, 
+                        message, 
+                "Localizer", 
+                        severity, 
+                        true),
+                    null));
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            _context = context;
+            string assemblyName = context.Compilation.AssemblyName ?? "Localizer";
+
+            var localizationFile = context.AdditionalFiles.FirstOrDefault(x =>
+                x.Path.EndsWith("localization.json", StringComparison.OrdinalIgnoreCase));
+            
+            if (localizationFile is null || !File.Exists(localizationFile.Path))
+            {
+                Parallel.ForEach(_context.AdditionalFiles, file =>
+                {
+                    var match = _fileRegex.Match(file.Path);
+
+                    if (!match.Success)
+                        return;
+
+                    Dictionary<string, string> contents;
+
+                    try
+                    {
+                        contents = JsonSerializer.Deserialize<Dictionary<string, string>>(File.ReadAllText(file.Path));
+                    }
+                    catch (Exception ex)
+                    {
+                        ReportError(ex.Message);
+                        return;
+                    }
+
+                    var languageCode = match.Groups[0].Value.Replace(".json","");
+                    
+                    if(!_languages.Contains(languageCode))
+                        _languages.Add(languageCode);
+
+                    foreach (var pair in contents)
+                    {
+                        var translations = _output.GetOrAdd(pair.Key, new Dictionary<string, string>());
+
+                        _output.AddOrUpdate(pair.Key, new Dictionary<string, string>
+                        {
+                            [languageCode] = pair.Value
+                        },
+                        (dictKey, oldValue) => oldValue);
+
+                        var hash = _tracking.GetOrAdd(pair.Key, new HashSet<string>());
+
+                        if (!hash.Contains(languageCode))
+                            hash.Add(languageCode);
+                        
+                        if (!translations.ContainsKey(languageCode))
+                            translations.Add(languageCode, pair.Value);
+                        else
+                            ReportError($"{pair.Key} already contains a translation for {languageCode}", DiagnosticSeverity.Warning);
+                    }
+                });
+                
+                GenerateOutput();
+                return;
+            }
+
+            // single file processing
+            Dictionary<string, Dictionary<string, string>> singleFileContents =
+                JsonSerializer.Deserialize<Dictionary<string, Dictionary<String, string>>>(
+                    File.ReadAllText(localizationFile.Path));
+
+            foreach (var translation in singleFileContents)
+            {
+                var entry = _tracking.GetOrAdd(translation.Key, new HashSet<string>());
+                
+                foreach (var language in translation.Value.Keys)
+                {
+                    if (!_languages.Contains(language))
+                        _languages.Add(language);
+
+                    if (!entry.Contains(language))
+                        entry.Add(language);
+                }
+
+                if (!_output.ContainsKey(translation.Key))
+                    _output.TryAdd(translation.Key, translation.Value);
+                else
+                    ReportError($"{translation.Key} has already been defined", DiagnosticSeverity.Warning);
+            }
+            
+            GenerateOutput();
+        }
+
+        string EntryFor(KeyValuePair<string, Dictionary<string, string>> entry)
+        {
+            using StringWriter stringWriter = new StringWriter();
+            using IndentedTextWriter writer = new IndentedTextWriter(stringWriter);
+            
+            writer.Indent = 3;
+            
+            writer.WriteLine($"[{ResourceEnum}.{entry.Key}] = new Dictionary<string,string>()");
+            writer.WriteLine("{");
+            writer.Indent++;
+            var keys = entry.Value.Keys.ToList();
+
+            for (int i = 0; i < keys.Count; i++)
+            {
+                string comma = i == keys.Count - 1 ? "" : ",";
+                writer.WriteLine($"[\"{keys[i]}\"] = \"{entry.Value[keys[i]]}\"{comma}");
+            }
+            
+            writer.Indent--;
+            writer.WriteLine("}");
+            
+            return stringWriter.ToString();
+        }
+        
+        private void GenerateOutput()
+        {
+            // Find the keys that are missing certain translations
+            foreach (var entry in _tracking)
+            {
+                var differences = _languages.Except(entry.Value).ToArray();
+                
+                if(differences.Any())
+                    ReportError($"{entry.Key} is missing translations for {string.Join(", ", differences)}", DiagnosticSeverity.Warning);
+            }
+            
+            List<string> entries = new List<string>();
+            foreach (var entry in _output)
+                entries.Add(EntryFor(entry));
+            
+            string template = $@"
+namespace {_context.Compilation.AssemblyName ?? "Localizer"};
 using System.Collections.Generic;
 using System.Linq;
 
 ///<summary>
-/// Strongly typed approach to referencing key values in <c>localization.json</c>
+/// Strongly typed approach to referencing key values in language files
 ///</summary>
-public enum LocalizationResource
-{
-    %ENUMS%
-}
+public enum {ResourceEnum}
+{{
+    {string.Join(",\n\t", _output.Keys)}
+}}
 
 ///<summary>
-/// Languages discovered in <c>localization.json</c>
+/// Languages that were discovered during compilation
 ///</summary>
-public enum LocalizationLanguage
-{
-    %LANGS%
-}
+public enum {LanguageEnum}
+{{
+    {string.Join(",\n\t", _languages.Select(x=>x.Replace("-","_")))}
+}}
 
 public static class Localizer
-{
-    private static Dictionary<LocalizationLanguage, string> _supportedLanguages = new Dictionary<LocalizationLanguage, string>()
-    {
-%SUPPORTED_LANGUAGES%
-    };
+{{
+    private static Dictionary<{LanguageEnum}, string> _supportedLanguages = new Dictionary<{LanguageEnum}, string>()
+    {{
+        {string.Join(",\n\t\t", _languages.Select(x=>$"[{LanguageEnum}.{x.Replace("-","_")}] = \"{x}\""))}
+    }};
 
-    private static Dictionary<LocalizationResource, Dictionary<string, string>> _resources =
-        new Dictionary<LocalizationResource, Dictionary<string, string>>()
-        {
-%ENTRIES%
-        };
-    
+    private static Dictionary<{ResourceEnum}, Dictionary<string,string>> _resources =
+        new Dictionary<{ResourceEnum}, Dictionary<string,string>>()
+        {{
+            {string.Join(",\n\t\t\t", entries)}
+        }};
+
     ///<summary>
-    /// List of languages supported / found in <c>localization.json</c>
+    /// List of languages supported / found during compilation
     ///</summary>
     public static List<string> SupportedLanguages => _supportedLanguages.Values.ToList();
 
     ///<summary>
     /// Translate <paramref name=""resource""/> into <paramref name=""language""/>
     ///</summary>
-    public static string Get(LocalizationResource resource, string language)
-    {
+    public static string Get({ResourceEnum} resource, string language)
+    {{
         if(!_resources.ContainsKey(resource) || !_resources[resource].ContainsKey(language))
             return string.Empty;
         
         return _resources[resource][language];
-    }
+    }}
 
     ///<summary>
     /// Translate <paramref name=""resource""/> into <paramref name=""language""/>
     ///</summary>
-    public static string Get(LocalizationResource resource, LocalizationLanguage language)
-    {
-        if(!_resources.ContainsKey(resource) || _supportedLanguages.ContainsKey(language))
-            return string.Empty;
-
+    public static string Get({ResourceEnum} resource, {LanguageEnum} language)
+    {{
+        if(!_resources.ContainsKey(resource) || !_resources[resource].ContainsKey(_supportedLanguages[language]))
+            return Get(resource);
+        
         return _resources[resource][_supportedLanguages[language]];
-    }
+    }}
 
     ///<summary>
     /// Get translation based on current culture
     ///</summary>
-    public static string Get(LocalizationResource resource)
-    {
+    public static string Get({ResourceEnum} resource)
+    {{
         string lang = System.Globalization.CultureInfo.CurrentCulture.Name;
-        return Get(resource, lang);
-    }
-    
+        return Get(resource,lang);
+    }}
+
     ///<summary>
     /// Translate <paramref name=""resource""/> using provided culture <paramref name=""info""/>
     ///</summary>
-    public static string Get(LocalizationResource resource, System.Globalization.CultureInfo info)
-    {
+    public static string Get({ResourceEnum} resource, System.Globalization.CultureInfo info)
+    {{
         return Get(resource, info.Name);
-    }
-}";
-
-        private readonly string _enumStart = new string('\t', 3);
-        readonly string _entryStartTab = new string('\t', 4);
-        readonly string _entryDictTab = new string('\t', 5);
-        readonly string _entryDictItemTab = new string('\t', 6);
-        private readonly HashSet<string> _languages = new HashSet<string>();
-
-        public void Initialize(GeneratorInitializationContext context)
-        {
-            
-        }
-        
-        Dictionary<string, Dictionary<string, string>> ParseFile(string jsonContents)
-        {
-            if (jsonContents is null)
-                throw new Exception("Unable to do anything");
-
+    }}
+}}";
             try
             {
-                return JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>>(jsonContents);
+                _context.AddSource("Localizer_generated.cs", template);
             }
             catch (Exception ex)
             {
-                ReportError(ex.Message);
-                throw;
-            }
-        }
-
-        string EntryFor(KeyValuePair<string, Dictionary<string, string>> entry)
-        {
-            StringBuilder results = new StringBuilder();
-            results.AppendLine($"{_entryStartTab}{{LocalizationResource.{entry.Key}, new Dictionary<string,string>()\n{_entryDictTab}{{");
-            
-            List<string> lines = new List<string>();
-
-            foreach (var pair in entry.Value)
-            {
-                if (!_languages.Contains(pair.Key))
-                    _languages.Add(pair.Key);
-                
-                lines.Add($"{_entryDictItemTab}[\"{pair.Key}\"] = \"{pair.Value}\"");
-            }
-            
-            results.AppendLine(string.Join(",\n", lines));
-            
-            results.AppendLine($"{_entryDictTab}}}\n{_entryStartTab}}}");
-            return results.ToString();
-        }
-
-        void ReportError(string message)
-        {
-            _context.ReportDiagnostic(Diagnostic.Create(new DiagnosticDescriptor("SI001", message, 
-                message, 
-                "Localizer", 
-                DiagnosticSeverity.Error, true), null));
-        }
-        
-        public void Execute(GeneratorExecutionContext context)
-        {
-            _context = context;
-            
-            string assemblyName = context.Compilation.AssemblyName ?? "Localizer";
-            
-            var localizationFile = context.AdditionalFiles.FirstOrDefault(x =>
-                x.Path.EndsWith("localization.json", StringComparison.InvariantCultureIgnoreCase));
-            
-            if (localizationFile is null || !File.Exists(localizationFile.Path))
-            {
-                ReportError("Unable to find localization.json");
-                return;
-            }
-                        
-            string contents = File.ReadAllText(localizationFile.Path);
-            
-            var data = ParseFile(contents);
-            
-            List<string> entries = new List<string>();
-            foreach (var entry in data)
-                entries.Add(EntryFor(entry));
-            
-            // Take the languages and convert them into enums!
-            List<string> languageDictionary = new List<string>();
-            List<string> languageEnums = new List<string>();
-
-            foreach (var lang in _languages)
-            {
-                // Sanitize text into something that conforms to C# syntax
-                var enumText = lang.Replace("-", "_");
-                languageDictionary.Add($"{_entryStartTab}[LocalizationLanguage.{enumText}] = \"{lang}\"");
-                languageEnums.Add(enumText);
-            }
-
-            string templateContents = Template
-                .Replace(NAMESPACE, assemblyName)
-                .Replace(ENUMS, string.Join($",\n{_enumStart}", data.Keys))
-                .Replace(ENTRIES, string.Join($"{_entryStartTab},\n", entries))
-                .Replace(SUPPORTED_LANGUAGES, string.Join($"{_entryStartTab},\n", languageDictionary))
-                .Replace(LANGUAGE_ENUMS, string.Join($",\n{_enumStart}", languageEnums));
-
-
-            try
-            {
-                context.AddSource($"Localizer_generated.cs", templateContents);
-            }
-            catch (Exception ex)
-            {
-                ReportError($"{ex.Message},\n\n{templateContents}");
+                ReportError($"{ex.Message},\n\n{template}");
             }
         }
     }
